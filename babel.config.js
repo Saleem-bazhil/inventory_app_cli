e7dbd209b6fec13e5490d43fdd113579faf782eb
Saleem-bazhil <ab588538@gmail.com>
2026-03-28 13:25:16 +0530

    fix the error

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  presets: [require('@react-native/babel-preset')],
+  plugins: [
+    require('react-native-css-interop/dist/babel-plugin').default,
+    [
+      require('@babel/plugin-transform-react-jsx'),
+      {
+        runtime: 'automatic',
+        importSource: 'react-native-css-interop',
+      },
+    ],
+  ],
 };
